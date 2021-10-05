58a7bd2a8aa0cc57918321d33cc706b0843faceb
demongki <86837780+demongki@users.noreply.github.com>
2021-10-05 12:38:08 +0700

    Add files via upload

---
+++ message/help.js
@@ -0,0 +1,170 @@
+exports.menu = (prefix, emote) => {
+    return `❐──➨ *「 LIST MENU 」*
+    
+        *BOT UPDATE RUNNING*
+        
+✗⃝🌍 _${prefix}topbalance_
+✗⃝🌍 _${prefix}glimit_
+✗⃝🌍 _${prefix}buylimit_ <angka>
+✗⃝🌍 _${prefix}buyglimit_ <angka>
+✗⃝🌍 _cekprefix_
+
+
+       🕊️ *TOOLS SECTION DISPLAY*
+       
+✗⃝🕊️ _${prefix}sticker_
+✗⃝🕊️ _${prefix}swm_
+✗⃝🕊️ _${prefix}take_
+✗⃝🕊️ _${prefix}kontak_ *nomor|nama*
+✗⃝🕊️ _${prefix}toimg_
+✗⃝🕊️ _${prefix}attp_ *text*
+✗⃝🕊️ _${prefix}amongus_ *text*
+✗⃝🕊️ _${prefix}codetext_ *text*
+✗⃝🕊️ _${prefix}translate_ *kodebahasa teks/reply*
+
+
+        🛰️ *MODERATION GRUP*
+
+✗⃝🛰 ️_${prefix}antilink_
+✗⃝🛰 ️_${prefix}antivo_
+✗⃝🛰️ _${prefix}welcome_
+✗⃝🛰️ _${prefix}left_
+✗⃝🛰️ _${prefix}antibadword_
+✗⃝🛰️ _${prefix}listbadword_
+✗⃝🛰️ _${prefix}addbadword_
+✗⃝🛰 ️_${prefix}delbadword_
+ 
+ 
+         🔮 *DOWNLOADER*
+         
+✗⃝🔮 _${prefix}ytmp4_ *url*
+✗⃝🔮 _${prefix}ytmp3_ *url*
+✗⃝🔮 _${prefix}igdl_ *url*
+✗⃝🔮 _${prefix}fbdl_ *url*
+✗⃝🔮 _${prefix}gogimage_ *query*
+✗⃝🔮 _${prefix}tiktok_ *url*
+✗⃝🔮 _${prefix}yts_ *query*
+✗⃝🔮 _${prefix}play_ *query*
+✗⃝🔮 _${prefix}playmp4_ *query*
+✗⃝🔮 _${prefix}igstalk_ *username*
+✗⃝🔮 _${prefix}ghstalk_ *username*`
+}
+
+exports.ownerr = (prefix, owner, emote) => {
+     return `❐──➨ *「 OWNER 」*
+✗⃝🌍 _> eval_
+✗⃝🌍 _$ execot_
+✗⃝🌍 _${prefix}self_
+✗⃝🌍 _${prefix}public_
+✗⃝🌍 _${prefix}restart
+✗⃝🌍 _${prefix}off_
+✗⃝🌍 _${prefix}addupdate_
+✗⃝🌍 _${prefix}resetlimit_
+✗⃝🌍 _${prefix}setpp_
+✗⃝🌍 _${prefix}setname_
+✗⃝🌍 _${prefix}setbio_
+✗⃝🌍 _${prefix}setprefix_
+✗⃝🌍 _${prefix}bc_
+✗⃝🌍 _${prefix}bcbuton_
+✗⃝🌍 _${prefix}block_
+✗⃝🌍 _${prefix}unblock_
+✗⃝🌍 _${prefix}ban_
+✗⃝🌍 _${prefix}unban_
+✗⃝🌍 _${prefix}clearall_
+✗⃝🌍 _${prefix}givelimit_
+✗⃝🌍 _${prefix}giveglimit_
+✗⃝🌍 _${prefix}exif_ *nama|author*
+✗⃝🌍 _${prefix}addsewa_ *link waktu*
+✗⃝🌍 _${prefix}delsewa_ *idgroup*
+✗⃝🌍 _${prefix}addprem_
+✗⃝🌍 _${prefix}delprem_`}
+
+exports.infobot = (botName, pendaftar) => {
+     return `*Information Bot WhatsApp*
+• *Bot name* : _${botName}_
+• *Number* :  @62877050482351
+• *owner name* : _ᗩᴋɪʀᴀᗷoтz〆⁩_
+• *Number* : @62877050482351
+• *Total users* : ${pendaftar.length} _Users_
+`}
+
+
+exports.help = (ucapanWaktu, timeWib, timeWita, timeWit, sender, email, owner, ig) => {
+     return `${ucapanWaktu} @${sender.split("@")[0]} 👋
+
+✗⃝🌍 *Date* : @${sender.split("@")[0]}
+     
+     _${timeWib} Wib_
+     _${timeWita} Wita_
+     _${timeWit} Wit_ 
+  
+
+
+✗⃝💫Creator Bot
+@${owner}
+
+_*Special Thanks To :*_
+ • *Dinata*
+ • *Akira*
+ • *Zak*
+ • *Dz*
+ • *Fauzan*
+ • *Devil*
+ • *Penyedia Rest Api*
+
+
+┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+◩ _*NOTE USER BOT*_
+
+*•This Whatsapp Bot Is Still Under Development*
+
+*If You Are Using WhatsApp Mod* 
+  *Please Enter Command Like #allmenu*
+
+*• And I Thank You For* 
+  *My Team Who Have Taught Me About Bots*
+  
+*• If You Find A Bug*
+  *Please Contact Delevor Bot Whatsapp*`}
+
+exports.sewa = (ucapanWaktu, botName, owner) => {
+return `
+*❒─────⎔✗⃝🌹 「 SEWA BOT 」⎔──❒*
+ *➪✗⃝🌹* _*SEWABOT 5K/MINGGU*_ 
+ *➪✗⃝🌹* _*SEWABOT 10K/BULAN*_
+ *➪✗⃝🌹* _*SEWABOT 15K/3BULAN*_ 
+ *➪✗⃝🌹* _*PERMANENT? LU TOLOLZ*_ 
+*❒───⎔✗⃝🌹* 
+
+*❒─────⎔✗⃝🌹 「 PREMIUM 」⎔──❒*
+ *➪✗⃝🌹* _*PREMIUM 5K/MINGGU*_
+ *➪✗⃝🌹* _*PREMIUM 10K/BULAN*_
+ *➪✗⃝🌹* _*PREMIUM 15K/3BULAN*_
+ *➪✗⃝🌹* _*PERMANENT? LU TOLOLZ*_
+*❒───⎔✗⃝🌹*
+
+*❒─────⎔✗⃝🌹 「 SEWA & PREMIUM 」⎔──❒*
+ *➪✗⃝🌹* _*SEWABOT + PREMIUM 10K/MINGGU*_
+ *➪✗⃝🌹* _*SEWABOT + PREMIUM 20K/BULAN*_
+ *➪✗⃝🌹* _*SEWABOT + PREMIUM 25K/3BULAN*_
+ *➪✗⃝🌹* _*PERMANENT? LU TOLOLZ*_
+*❒───⎔✗⃝🌹*
+   
+*MINAT CHAT OWNER AKIRA BOT*
+@${owner}
+`}
+ 
+exports.rules = (prefix, sender) => {
+    return `
+*❒───⎔✗⃝🌹 「 RULES BOT 」⎔─❒*
+ *└───────*
+ *└✔️* Jangan Spam Bot. 
+  *└✔️* Sanksi: *WARN/SOFT BLOCK*
+   *└✔️* Jangan Telepon Bot.
+    *└✔️* Sanksi: *SOFT BLOCK*
+   *└✔️* Jangan Mengejek Bot.
+  *└✔️* Sanksi: *PERMANENT BLOCK*
+ *└❏*
+   *└* _Jika Sudah dipahami RulesNya, Silakan Ketik *.menu* untuk MeMulai Atau Pencet Menu Di Bawah!!!_
+*❒─⎔✗⃝🌹*
+`}
